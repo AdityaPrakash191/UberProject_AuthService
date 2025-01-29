23460551fcb1a435b6725ca2c6cbb3c48d5e5543
Aditya <adityamgs2018@gmail.com>
2025-01-30 02:04:31 +0530

    added extracting methods

--- src/main/java/com/example/UberProject_AuthService/services/JWTSecurity.java
+++ src/main/java/com/example/UberProject_AuthService/services/JWTSecurity.java
@@ -1,17 +1,22 @@
 package com.example.UberProject_AuthService.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JWTSecurity implements CommandLineRunner {
@@ -22,17 +27,17 @@ public class JWTSecurity implements CommandLineRunner {
     @Value("${jwt.expiry}")
     private int expiry;
 
-    private String createToken(Map<String,Object> payload , String username){
+    private String createToken(Map<String,Object> payload , String email){
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + expiry*1000L);
-        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
 
         return Jwts.builder()
                 .claims(payload)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(expiryDate)
-                .subject(username)
-                .signWith(key)
+                .subject(email)
+                .signWith(getSignKey())
                 .compact();
 
     }
@@ -44,8 +49,39 @@ public class JWTSecurity implements CommandLineRunner {
         String token = createToken(m,"sanket");
         System.out.println("The generated token is " + token);
 
-
-
     }
 
+    private Claims extractAllPayload(String token) {
+        return Jwts
+                .parser()
+                .verifyWith((getSignKey()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public <T> T extractClaim(String token , Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllPayload(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private String extractEmail(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    private Boolean validateToken(String email, String token){
+        String userEmailFetchedFromToken = extractEmail(token);
+        return (userEmailFetchedFromToken.equals(email) && !isTokenExpired(token));
+    }
 }
